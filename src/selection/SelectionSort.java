5d5da029ed98bbc9821ca13cb05cd3766b7f8f12
jkwcp <jk.cpwang@gmail.com>
2019-07-02 17:32:18 +0800

    Selection sort sample code

---
+++ src/selection/SelectionSort.java
@@ -0,0 +1,34 @@
+package selection;
+
+// Sample codes
+// Selection sort: go through the array and select the min of the remainings, swap that with the first
+public class SelectionSort  {
+
+    public static void sort(Comparable[] a){
+        int N = a.length;
+        for (int i = 0; i < N - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++){
+                if (less(min, a[j]))
+                    min = j;
+            }
+
+            if (i != min) {
+                exch(a, i, min);
+            }
+        }
+
+    }
+
+    private static boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+}
